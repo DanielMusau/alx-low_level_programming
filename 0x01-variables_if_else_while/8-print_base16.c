84016c8e94b583d2da817de6287dad4a140ed001
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-13 09:37:12 -0700

    Print Hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all hexadecimal numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n < 103; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
