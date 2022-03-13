01572858495f4f9ddf0b7b405ad58a2aadb2ac40
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-13 08:23:35 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int n;
+
+	for (n = 0; n < 26; n++)
+	{
+		putchar(alph[n]);
+	}
+	putchar('\n');
+	return (0);
+}
