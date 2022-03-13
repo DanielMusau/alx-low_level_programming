6f9c8cf46cb2f6c6518701cafe064ec589ab5f3d
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-13 09:14:32 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
