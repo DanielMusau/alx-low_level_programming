ee18725bf9994b261814c18c0ae091daf9d47786
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-15 06:59:54 -0700

    Print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
