13096297c9d119e87e578196a9e2ea1af860a2c1
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-15 07:53:18 -0700

    check for lowercase letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,13 @@
 /**
  * _islower - Checks for lowercase letters.
  *
+ * @c: The character in ASCII code
+ *
  * Return: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
 {
-	if(c >= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
