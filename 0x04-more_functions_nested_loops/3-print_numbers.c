5608560353fdf5828b1ece7ffe135ea1546ff0b1
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-17 06:36:26 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9.
+ *
+ * Return: no return.
+ */
+
+void print_numbers(void)
+{
+	int l;
+
+	for (l = 48; l < 58; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
