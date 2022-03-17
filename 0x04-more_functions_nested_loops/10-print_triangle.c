2bea0d8b1fe69336b6ca224f8aa71134fcb62741
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-17 08:25:29 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle
+ * @size: size of the triangle.
+ *
+ * Return: no return.
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+		{
+			_putchar(' ');
+		}
+		for (j--; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+
+}
