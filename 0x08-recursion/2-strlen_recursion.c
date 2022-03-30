b2b95794b66afd79d4d88c5b320f369af6ea9953
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-03-30 07:55:38 -0700

    Length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: string.
+ *
+ * Return: length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return(1 + _strlen_recursion(s + 1));
+}
