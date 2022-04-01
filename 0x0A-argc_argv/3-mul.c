8ce8f5c6853587af8ef9c74e5ed1cde6c5541c5d
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-01 07:44:41 -0700

    Multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Multiplies 2 numbers.
+ * @argc: Number of comand line arguments.
+ * @argv: Array of command line arguments.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int count, mul = 1;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			mul = mul * atoi(argv[count]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
