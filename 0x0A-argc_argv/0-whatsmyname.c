30e1ddf15d2fa190f01469dcdf6890dd25eb8d22
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-01 07:12:37 -0700

    My name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc __attribute__((unused)), char* argv[])
+/**
+ * main - prints its name follwed by a new line.
+ * @argc: Number of command line arguments.
+ * @argv: 1-dimensional array that contains the comand line arguments.
+ * Return: 0.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	int i = 0;
+
 	printf("%s\n", argv[i]);
 	return (0);
 }
