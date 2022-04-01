90a3a524a0943eb8071d831df06ab50e946ce10c
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-01 09:05:59 -0700

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,12 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 1 && argc <= 3)
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
