220cc170173718165ad6fb62386b4fb339abbeb8
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-07 04:58:06 -0700

    Concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,12 +18,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
-	for (ls2 = 0; s1[ls2] != '\0'; ls2++)
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
 
 	if (n > ls2)
