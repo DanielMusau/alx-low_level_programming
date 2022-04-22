cacc518f4328fc7c8fd8d69b19b6a4fbf8566ede
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-22 07:41:30 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list of nodes.
+ * @head: pointer to the first element of the lists.
+ * Return: Nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head->str);
+	free(head);
+}
