f008337857538aa58e5323e458cf234b2fb7aa62
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-26 07:01:24 -0700

    Fixed the pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return (0);
 	}
@@ -24,4 +24,5 @@ int pop_listint(listint_t **head)
 	}
 
 	return (n);
+
 }
