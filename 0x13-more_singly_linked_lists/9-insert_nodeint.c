797f9026c128bd9155f2d3d287d3fb623ba57428
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-26 10:03:40 -0700

    Add at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+listint_t *create_node(const int n);
 
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position.
@@ -10,5 +11,38 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *temp, *new_node, *temp_old;
+	unsigned int count = 0;
 
+	temp = *head;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = create_node(n);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	if (idx == 0)
+		*head = new_node;
+
+	for (count = 0; count < idx - 1 && temp != NULL && idx != 0; count++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+	if (idx == 0)
+		new_node->next = temp;
+	else
+	{
+		temp_old = temp->next;
+		temp->next = new_node;
+		new_node->next = temp_old;
+	}
+	return (new_node);
 }
