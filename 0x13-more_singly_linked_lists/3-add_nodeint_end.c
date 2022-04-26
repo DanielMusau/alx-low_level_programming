c4208345fbe59718b91afcaaca6f966467dc0f09
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-26 09:03:48 -0700

    Fixed changes to the add node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+listint_t *create_node(const int n);
 
 /**
  * add_nodeint_end - Adds a new node at the end of a list.
@@ -15,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	new_node = (listint_t*)malloc(sizeof(listint_t));
+	new_node = create_node(n);
 
 	if (new_node == NULL)
 		return (NULL);
@@ -25,12 +26,30 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 		return (*head);
 	}
-	new_node->n = n;
 
 	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
 	}
 	ptr->next = new_node;
+
 	return (*head);
+}
+
+/**
+ * create_node - Creates a node.
+ * @n: value for the node.
+ * Return: A pointer to the beginning of the list.
+ */
+listint_t *create_node(const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	return (new_node);
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: pointer to the beginning node.
+ * @idx: Index where the new node should be added.
+ * @n: The data to be added.
+ *
+ * Return: the address of the new node, or NULL if failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+}
