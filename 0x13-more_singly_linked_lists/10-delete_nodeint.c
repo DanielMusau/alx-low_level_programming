592f58230ae6fb7cbafa44f2e9d83fdd2fd18516
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-26 10:26:33 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	temp = *head;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	while (temp != NULL)
