a045d7b119299d405af905e2a0bfe1d0b5b742f2
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-26 06:14:36 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list.
+ * @head: pointer to the beginning of the node.
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *aux;
+
+	ptr = *head;
+
+	while (ptr != NULL)
+	{
+		aux = ptr;
+		ptr = ptr->next;
+		free(aux);
+	}
+
+	*head = NULL;
+}
