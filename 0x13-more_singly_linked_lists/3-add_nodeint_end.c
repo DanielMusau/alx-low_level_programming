a4a00180eb66cfede0e100ce5eac7f651d8fca37
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-26 09:05:21 -0700

    Fixed changes to the add node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (*head);
