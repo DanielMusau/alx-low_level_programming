325cd8f384eb28425eb314025311a6dd0365cf15
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-27 04:12:16 -0700

    Fixed the delete node function

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,19 +11,35 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int count = 0;
 	listint_t *temp;
+	listint_t *node;
 
 	temp = *head;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	while (temp != NULL)
+	while (temp != NULL && count < index - 1 && index != 0)
 	{
-		if (count == index)
-			free(temp);
 		count++;
 		temp = temp->next;
 	}
 
+	if (temp == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		node = temp->next;
+		free(temp);
+		*head = node;
+	}
+	else
+	{
+		if (temp->next == NULL)
+			node = temp->next;
+		else
+			node = temp->next->next;
+		free(temp->next);
+		temp->next = node;
+	}
 	return (1);
 }
