15d2f102f5c36c45a3927f904f379d4fa0141e53
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-04-29 03:51:30 -0700

    Value at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: Number to be converted.
+ * @index: is the index starting from 0.
+ *
+ * Returns: the value of the bit at index or -1 if an error occcured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 64)
+		return (-1);
+
+	for (i = 0; i < index; i++)
+		n = n >> 1;
+	return ((n & 1));
+}
