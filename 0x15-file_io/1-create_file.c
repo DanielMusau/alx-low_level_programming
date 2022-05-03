f1f868e318e4e31f5605b9004d69ef84add07a49
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-05-03 05:23:24 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDWR, | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
