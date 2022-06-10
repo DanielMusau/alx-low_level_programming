ee2eb3b6d72dda7dc2c7df5a3404506050fae25c
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-10 03:50:51 -0700

    Free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,13 +6,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp = head;
+	dlistint_t *temp;
 
 	if (head != NULL)
 		while (head->prev != NULL)
 			head = head->prev;
 
-	while (temp != NULL)
+	while ((temp = head) != NULL)
 	{
 		head = head->next;
 		free(temp);
