10253972bafc55dc2605861613557a8714f54045
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-07-06 11:51:44 -0700

    hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,4 +42,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 hash_node_t *addn_hash(hash_node_t **, const char *, const char *);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
