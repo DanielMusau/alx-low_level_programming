7e18c974dc95e2389dfb699121a82b16097a6bc8
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-02-03 18:01:28 +0300

    Creating Hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		table->array[i] = NULL;
 
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t **));
+	table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * table->size);
 
 	return (table);
 }
