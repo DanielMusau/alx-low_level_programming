cf6820239f80a5d648d8578773abb4b28fb3b5f2
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-02-03 19:14:38 +0300

    Fixing hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,4 +36,30 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 
+/* 2-key_index.c */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+/* 3-hash_table_set.c */
+hash_node_t *add_n_hash(hash_node_t **, const char *, const char *);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+/* 4-hash_table_get.c */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
+/* 5-hash_table_print.c */
+void hash_table_print(const hash_table_t *ht);
+
+/* 6-hash_table_delete.c */
+void hash_table_delete(hash_table_t *ht);
+
+/* 100-sorted_hash_table.c */
+shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *add_n_shash(shash_node_t **, const char *, const char *);
+void add_i_shash(shash_table_t *ht, shash_node_t *new);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
 #endif
