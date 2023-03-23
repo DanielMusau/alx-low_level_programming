29d8e87dec23388eefc32d804bc424d808c44881
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-03-23 13:55:27 +0300

    Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module contains a function that returns the perimeter
+of the island described in grid."""
+
+
+def island_perimeter(grid):
+    """Function that returns the perimeter of
+    island described in grid."""
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+
+    return perimeter
