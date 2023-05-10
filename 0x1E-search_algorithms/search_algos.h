59e8e4636b498061a5a88370cf93d9331038c88b
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-05-10 16:00:41 +0300

    Jump search on linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,22 @@
 #include <stddef.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -12,4 +28,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 void print_array(int *array, size_t size);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 #endif
